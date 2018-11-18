8d3e0af47444265ecb26777878b961761e64289b
Janujan Selva <janujanselva@janujans-macbook-pro.local>
2018-11-18 03:18:48 -0500

    testing player class

--- main.cpp
+++ main.cpp
@@ -7,9 +7,23 @@
 //
 
 #include <stdio.h>
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include "player.hpp"
 
+using namespace std;
 int main()
 {
+    string name = "Jay";
+    Player P = Player(name);
     
+    cout <<"Player Name: " << P.getName() << endl;
+    
+    vector<int> piece_status = P.getPieces();
+    for(int i = 0; i < PIECE_NUM; i++)
+    {
+        cout << "Piece " << i + 1 << " " << piece_status[i] << endl;
+    }
     return 0;
 }
