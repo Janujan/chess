eac7c5528aaddc1680ea5411c5c0679c538903d8
Janujan Selva <janujanselva@janujans-macbook-pro.local>
2018-11-18 02:05:58 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chess
+//
+//  Created by Janujan Selva on 2018-11-18.
+//  Copyright © 2018 Janujan Selva. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    
+    return 0;
+}
